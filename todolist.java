fda5f9e4b935177f8659555fafcab9c32e1316ba
Matru Prasad Mishra <matru19042003@gmail.com>
2023-04-20 21:17:15 +0530

    initial commit

---
+++ todolist.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class first
+{
+ public static void main(String args[])
+ {
+  System.out.println("SWEET MESSAGE-  HELLO GOOD MORNING ,HERE IS YOUR TO-DO LIST :)");
+  
+  Scanner sc=new Scanner(System.in);
+
+  int n;
+  System.out.println("                                    ");
+  System.out.println("____________________________________");
+  System.out.println("                                    ");
+  System.out.print("PLEASE ENTER TOTAL AMOUNT OD WORK:    ");
+  n=sc.nextInt();
+
+  for(int i=0;i<=n;i++)
+  {
+   System.out.print("TODAYS JOBS:  ");
+   String a=sc.next();
+ }
+  System.out.println("HOPE YOU SASTISFIED BT YOUR  WORK   ");
+}}
+
